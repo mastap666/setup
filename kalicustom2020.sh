4a01eb5f63264397bd28d71adf4701aed2661659
Patrick Frei <Patrick@PNP.LOCAL>
2020-04-29 21:48:26 +0200

    Signed-off-by: Patrick Frei <Patrick@PNP.LOCAL>

--- kalicustom2020.sh
+++ kalicustom2020.sh
@@ -74,218 +74,218 @@ if [ $update == y ]
 			rolling="n"
 		
 fi		
-echo "
-
-" 
-
-
-#### main script #####
-
-# expand the ls command
-sleep 2
-
-echo "alias ls='ls -la --color=auto'" >>~/.bashrc
-alias ls='ls -la --color=auto'
-
-
-
-# set keyboard layout
-setxkbmap $keyboard
-echo "setxkbmap $keyboard" >>~/.bashrc
-
-# set wallpaper and disable lockscreen
-cd /usr/share/backgrounds/
-wget https://raw.githubusercontent.com/mastap666/scripts/master/wallpaper/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/last-image --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/last-single-image --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/image-path --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/last-image --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/last-single-image --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual1/workspace0/last-image --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual1/workspace1/last-image --set /usr/share/backgrounds/wp1.jpg
-
-
-#set new root password
-if [ $rootpw == y ]
-		then
-			echo "
-
-change root password...
--------------------
-
-"
-			sleep 2
-			passwd root
-			sleep 5
-			
-fi
-echo "
-
-"
-
-#set new kali password
-if [ $kalipw == y ]
-		then
-			echo "
-
-change kali User password...
--------------------
-
-"
-			sleep 2
-			passwd kali
-			sleep 5
-			
-fi
-echo "
-
-"
-
-#install tools
-echo "Default tools will be installed...
-----------------------------------
-"
-sleep 3
-apt-get update
-sleep 3
-echo "ntpdate"
-apt-get install ntpdate -y
-#apt-get install terminator -y
-echo "tools"
-cd /
-mkdir tools
-
-#update tools
-# nmap --script-updatedb
-# searchsploit -u
-
-
-#Github Clones
-if [ $git == "y" ]
-		then 
-			echo "
-			
-Github Tools will be cloned...
-----------------------------------
-
-"
-sleep 3
-
-mkdir /tools/gitclone
-cd /tools/gitclone
-git clone https://github.com/OCSAF/freevulnsearch.git 
-git clone https://github.com/OCSAF/freevulnaudit.git
-git clone https://github.com/OCSAF/free-open-auditor.git
-git clone https://github.com/OCSAF/freepwnedcheck.git
-
-
-
-
-if [ $tools == "y" ]
-		then 
-			echo "
-			
-Custom tools will be installed...
-----------------------------------
-
-			"
-			sleep 3
-			#div_tools
-			apt-get install ipcalc -y
-			apt-get install mtr -y
-			apt-get install mtr-tiny -y
-			apt-get install tor -y
-			apt-get install openvas -y
-			#freevulnserach
-			apt-get install geoip-bin -y
-			apt-get install jq -y
-			#freevulnaudit
-			apt-get install xlstproc -y
-			apt-get install wkhtmltopdf -y
-			echo "
-			
-tool installations completed...
-
-"
-
-
-
-#set ntp server
-echo "
-
-Set ntp server...
------------------
-"
-ntpdate $ntp
-sleep 3
-
-# change hostname
-if [ $hs == y ]
-		then
-			echo "
-
-Set new hostname...
------------------
-"
-			echo $hostname >/etc/hostname
-			echo "
-			
-Your new hostname is $hostname
-
-"
-
-fi
-
-# enable ssh with root access
-if [ $ssh == "y" ]
-        then
-		echo "
-
-Setup SSH Server...
--------------------
-
-"
-			echo SSH will be enabled...
-			echo "PubkeyAuthentication yes" >>/etc/ssh/sshd_config
-			systemctl enable ssh
-			systemctl start ssh
-
-fi
-echo "
-"
-sleep 3
-# set timezone
-timedatectl set-timezone $timezone
-
-
-
-#DNS Quad9 Server
-if [ $dns == y ]
-		then
-				echo "Quad9 DNS will be configured... "
-				echo "nameserver 9.9.9.9" >/etc/resolv.conf
-				
-fi
-
-# update kali
-if [ $update == y ]
-        then
-                echo "Updates will be installed, please hold the line..."
-				sleep 3
-                apt-get update -y && apt-get upgrade -y
-				
-	elif [ $rolling == y ]
-		then
-				echo "Rolling updates will be installed..."
-				apt-get dist-upgrade -y
-                
-	else
-                echo "No updates will be installed..."
-
-fi
-
-
+#echo "
+#
+#" 
+#
+#
+##### main script #####
+#
+## expand the ls command
+#sleep 2
+#
+#echo "alias ls='ls -la --color=auto'" >>~/.bashrc
+#alias ls='ls -la --color=auto'
+#
+#
+#
+## set keyboard layout
+#setxkbmap $keyboard
+#echo "setxkbmap $keyboard" >>~/.bashrc
+#
+## set wallpaper and disable lockscreen
+#cd /usr/share/backgrounds/
+#wget https://raw.githubusercontent.com/mastap666/scripts/master/wallpaper/wp1.jpg
+#xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /usr/share/backgrounds/wp1.jpg
+#xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/last-image --set /usr/share/backgrounds/wp1.jpg
+#xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/last-single-image --set /usr/share/backgrounds/wp1.jpg
+#xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/image-path --set /usr/share/backgrounds/wp1.jpg
+#xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/last-image --set /usr/share/backgrounds/wp1.jpg
+#xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/last-single-image --set /usr/share/backgrounds/wp1.jpg
+#xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual1/workspace0/last-image --set /usr/share/backgrounds/wp1.jpg
+#xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual1/workspace1/last-image --set /usr/share/backgrounds/wp1.jpg
+#
+#
+##set new root password
+#if [ $rootpw == y ]
+#		then
+#			echo "
+#
+#change root password...
+#-------------------
+#
+#"
+#			sleep 2
+#			passwd root
+#			sleep 5
+#			
+#fi
+#echo "
+#
+#"
+#
+##set new kali password
+#if [ $kalipw == y ]
+#		then
+#			echo "
+#
+#change kali User password...
+#-------------------
+#
+#"
+#			sleep 2
+#			passwd kali
+#			sleep 5
+#			
+#fi
+#echo "
+#
+#"
+#
+##install tools
+#echo "Default tools will be installed...
+#----------------------------------
+#"
+#sleep 3
+#apt-get update
+#sleep 3
+#echo "ntpdate"
+#apt-get install ntpdate -y
+##apt-get install terminator -y
+#echo "tools"
+#cd /
+#mkdir tools
+#
+##update tools
+## nmap --script-updatedb
+## searchsploit -u
+#
+#
+##Github Clones
+#if [ $git == "y" ]
+#		then 
+#			echo "
+#			
+#Github Tools will be cloned...
+#----------------------------------
+#
+#"
+#sleep 3
+#
+#mkdir /tools/gitclone
+#cd /tools/gitclone
+#git clone https://github.com/OCSAF/freevulnsearch.git 
+#git clone https://github.com/OCSAF/freevulnaudit.git
+#git clone https://github.com/OCSAF/free-open-auditor.git
+#git clone https://github.com/OCSAF/freepwnedcheck.git
+#
+#
+#
+#
+#if [ $tools == "y" ]
+#		then 
+#			echo "
+#			
+#Custom tools will be installed...
+#----------------------------------
+#
+#			"
+#			sleep 3
+#			#div_tools
+#			apt-get install ipcalc -y
+#			apt-get install mtr -y
+#			apt-get install mtr-tiny -y
+#			apt-get install tor -y
+#			apt-get install openvas -y
+#			#freevulnserach
+#			apt-get install geoip-bin -y
+#			apt-get install jq -y
+#			#freevulnaudit
+#			apt-get install xlstproc -y
+#			apt-get install wkhtmltopdf -y
+#			echo "
+#			
+#tool installations completed...
+#
+#"
+#
+#
+#
+##set ntp server
+#echo "
+#
+#Set ntp server...
+#-----------------
+#"
+#ntpdate $ntp
+#sleep 3
+#
+## change hostname
+#if [ $hs == y ]
+#		then
+#			echo "
+#
+#Set new hostname...
+#-----------------
+#"
+#			echo $hostname >/etc/hostname
+#			echo "
+#			
+#Your new hostname is $hostname
+#
+#"
+#
+#fi
+#
+## enable ssh with root access
+#if [ $ssh == "y" ]
+#        then
+#		echo "
+#
+#Setup SSH Server...
+#-------------------
+#
+#"
+#			echo SSH will be enabled...
+#			echo "PubkeyAuthentication yes" >>/etc/ssh/sshd_config
+#			systemctl enable ssh
+#			systemctl start ssh
+#
+#fi
+#echo "
+#"
+#sleep 3
+## set timezone
+#timedatectl set-timezone $timezone
+#
+#
+#
+##DNS Quad9 Server
+#if [ $dns == y ]
+#		then
+#				echo "Quad9 DNS will be configured... "
+#				echo "nameserver 9.9.9.9" >/etc/resolv.conf
+#				
+#fi
+#
+## update kali
+#if [ $update == y ]
+#        then
+#                echo "Updates will be installed, please hold the line..."
+#				sleep 3
+#                apt-get update -y && apt-get upgrade -y
+#				
+#	elif [ $rolling == y ]
+#		then
+#				echo "Rolling updates will be installed..."
+#				apt-get dist-upgrade -y
+#                
+#	else
+#                echo "No updates will be installed..."
+#
+#fi
+#
+#
 echo "
 
 
