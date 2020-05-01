65bd9a75be9b38b60c69850a3b92deb6401ff15b
Patrick Frei <Patrick@PNP.LOCAL>
2020-05-01 11:35:01 +0200

    Signed-off-by: Patrick Frei <Patrick@PNP.LOCAL>

--- kalicustom2020.sh
+++ kalicustom2020.sh
@@ -13,10 +13,12 @@
 #2.00	New version for Kali 2020
 #2.01	Bugfix
 #2.10	updated  script
+#2.20	rebuild script
 ###
 
 
-# Title
+#### Title ####
+
 clear
 
 echo "
@@ -28,7 +30,7 @@ echo "
   |  .  ||  |  ||     | |  |     \     ||     |\    |  |  |  |     ||   |   |     \    \     ||  .  \ |  | |  |    |  |  
   |__|\_||__|__||_____||____|     \____| \__,_| \___|  |__|   \___/ |___|___|      \___|\____||__|\_||____||__|    |__|  
    
-                                  --== V2020 by Patrick Frei - v.2.10 - APR2020 ==--
+                                  --== V2020 by Patrick Frei - v.2.20 - MAY2020 ==--
 											
 
 
@@ -42,248 +44,382 @@ Run this script as sudo -i
  
 "
 
-#### Check root rights (sudo) before execution ####
+#### Check root rights (sudo) ####
 if [ $(id -u) -ne 0 ]; then
 	echo "You need root rights (sudo)."
 	exit
 fi
 
 
-#### Variables ######
-read -p "Choose your keyboard layout (ch;de;us): " -i ch -e keyboard
-read -p "Choose your system time zone (Europe/Zurich): " -i Europe/Zurich -e timezone
-read -p "Choose your ntp server: " -i ch.pool.ntp.org -e ntp
-read -p "Clone Github Tools? (y/n): " -i y -e git
-read -p "Install Custom Tools? (y/n): " -i y -e tools
-read -p "Change Hostname (y/n): " -i n -e hs
-if 	[ $hs == y ]
+### COLORS  ###
+
+#Colors directly in the script.
+
+cOFF='\e[39m'	  #color OFF / Default color
+rON='\e[31m'	  #red color ON
+gON='\e[32m'	  #green color ON
+yON='\e[33m'	  #yellow color ON
+
+
+#### Functions #####
+
+funcConfExpand(){
+	# expand the bashrc
+	echo "alias ls='ls -la --color=auto'" >>/etc/bash.bashrc
+
+	# expand the vimrc
+	echo "set number" >>/etc/vimrc
+	echo "set bg=dark" >>/etc/vimrc
+}
+
+funcKeylayout(){
+	# set keyboard layout
+	setxkbmap ${keyboard}
+	echo "setxkbmap $keyboard" >>/etc/bash.bashrc
+	echo ""
+	echo Keyboard Layout changed to ${_keyboard}
+	echo ""
+	sleep 2
+}
+
+funcSetwallpaper(){
+	# set wallpaper
+	cd /usr/share/backgrounds/
+	wget https://raw.githubusercontent.com/mastap666/scripts/master/wallpaper/wp1.jpg
+	su kali -m -c 'xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /usr/share/backgrounds/wp1.jpg
+	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/last-image --set /usr/share/backgrounds/wp1.jpg
+	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/last-single-image --set /usr/share/backgrounds/wp1.jpg
+	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/image-path --set /usr/share/backgrounds/wp1.jpg
+	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/last-image --set /usr/share/backgrounds/wp1.jpg
+	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/last-single-image --set /usr/share/backgrounds/wp1.jpg
+	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual1/workspace0/last-image --set /usr/share/backgrounds/wp1.jpg
+	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual1/workspace1/last-image --set /usr/share/backgrounds/wp1.jpg'
+}
+
+funcSetpw(){
+	#set new root password
+	if [ ${_rootpw} == y ]
+			then
+				echo "
+
+	change root password...
+	-------------------
+
+	"
+				passwd root
+				sleep 5
+				
+	fi
+	echo "
+
+	"
+
+	#set new kali password
+	if [ ${_kalipw} == y ]
+			then
+				echo "
+
+	change kali User password...
+	-------------------
+
+	"
+				passwd kali
+				sleep 5
+				
+	fi
+
+}
+
+funcDefaulttools(){
+	##install tools
+	echo "
+
+	Default tools will be installed...
+	----------------------------------
+	"
+	sleep 3
+	apt-get update
+	sleep 3
+	apt-get install ntpdate -y
+	apt-get install terminator -y
+	apt-get install tshark -y
+	mkdir -p /tools
+
+}
+
+funcUpdatetools(){
+	#update tools
+	nmap --script-updatedb
+	searchsploit -u
+}
+
+funcGithub(){
+	#Github Clones
+	if [ ${_git} == "y" ]
+			then 
+				echo "			
+	Github Tools will be cloned...
+	----------------------------------
+
+	"
+
+	mkdir -p /tools/gitclone
+	cd /tools/gitclone
+	git clone https://github.com/OCSAF/freevulnsearch.git 
+	git clone https://github.com/OCSAF/freevulnaudit.git
+	git clone https://github.com/OCSAF/free-open-auditor.git
+	git clone https://github.com/OCSAF/freepwnedcheck.git
+
+	fi
+}
+
+funcCustomtools(){
+	if [ ${_tools} == "y" ]
+			then 
+				echo "
+				
+	Custom tools will be installed...
+	----------------------------------
+
+				"
+				sleep 2
+				#div_tools
+				apt-get install ipcalc -y
+				apt-get install mtr -y
+				apt-get install mtr-tiny -y
+				apt-get install tor -y
+				apt-get install openvas -y
+				#freevulnserach
+				apt-get install geoip-bin -y
+				apt-get install jq -y
+				#freevulnaudit
+				apt-get install xlstproc -y
+				apt-get install wkhtmltopdf -y
+				echo "
+				
+	tool installations completed...
+
+	"
+}
+
+funcSetntp(){
+	#set ntp server
+	echo "
+
+	Set ntp server...
+	-----------------
+	"
+	ntpdate ${_ntp}
+	sleep 3
+}
+
+funcSethostname(){
+	# change hostname
+	if 	[ ${_hs} == y ]
+			then
+				read -p "Enter new hostname: " -e _hostname
+	fi
+
+	echo "
+	Set new hostname...
+	-----------------
+	"
+	echo ${_hostname} >/etc/hostname
+	echo "
+				
+	Your new hostname is ${_hostname}
+
+	"
+	sleep 3
+}
+
+funcSSHenable(){
+	# enable ssh with root access
+	if [ ${_ssh} == "y" ]
+			then
+			echo "
+
+	Setup SSH Server...
+	-------------------
+
+	"
+				echo SSH will be enabled...
+				echo "PubkeyAuthentication yes" >>/etc/ssh/sshd_config
+				systemctl enable ssh
+				systemctl start ssh
+
+	fi
+}
+
+funcTimezone(){
+	# set timezone
+	timedatectl set-timezone ${_timezone}
+}
+
+funcDNSchange(){
+	#DNS Quad9 Server
+	if [ ${_dns} == y ]
+			then
+					echo "Quad9 DNS will be configured... "
+					echo "nameserver 9.9.9.9" >/etc/resolv.conf
+					
+	fi
+}
+
+funcUpdate(){
+	# update kali
+	if [ ${_update} == y ]
+			then
+					echo "Updates will be installed, this takes some time...
+					"
+					apt-get update -y && apt-get upgrade -y
+					
+		elif [ ${_rolling} == y ]
+			then
+					echo "Rolling updates will be installed...
+					"
+					apt-get dist-upgrade -y
+					
+		else
+					echo "No updates will be installed...
+					"
+
+	fi
+}
+
+
+
+
+#### MAIN Script ######
+
+## Reads ##
+
+read -p "Choose your keyboard layout (ch;de;us): " -i ch -e _keyboard
+read -p "Choose your system time zone (Europe/Zurich): " -i Europe/Zurich -e _timezone
+read -p "Choose your ntp server: " -i ch.pool.ntp.org -e _ntp
+read -p "Clone Github Tools? (y/n): " -i y -e _git
+read -p "Install Custom Tools? (y/n): " -i y -e _tools
+read -p "Change Hostname (y/n): " -i n -e _hs
+read -p "Enable SSH access? (y/n): " -i n -e _ssh
+read -p "Change root password? (y/n): " -i n -e _rootpw
+read -p "Change kali user password? (y/n): " -i n -e _kalipw
+read -p "Change to Quad9 DNS Server? (y/n): " -i n -e _dns
+read -p "Install Kali updates? (y/n): " -i n -e _update
+if [ ${_update} == y ]
 		then
-			read -p "Enter new hostname: " -e hostname
-	else
-			hostname=kali
-fi
-read -p "Enable SSH access? (y/n): " -i n -e ssh
-read -p "Change root password? (y/n): " -i n -e rootpw
-read -p "Change kali user password? (y/n): " -i n -e kalipw
-read -p "Change to Quad9 DNS Server? (y/n): " -i n -e dns
-read -p "Install Kali updates? (y/n): " -i n -e update
-if [ $update == y ]
-		then
-			read -p "Install Kali rolling Updates? (y/n): " -i n -e rolling
-	else
-			rolling="n"
+			read -p "Install also Kali rolling Updates? (y/n): " -i n -e _rolling
+else
+			_rolling="n"
 		
 fi		
+
+
 echo "
+
 
 " 
-
-
-#### main script #####
-
-# expand the ls command
-sleep 2
-
-echo "alias ls='ls -la --color=auto'" >>~/.bashrc
-alias ls='ls -la --color=auto'
-
-
-
-# set keyboard layout
-setxkbmap $keyboard
-echo "setxkbmap $keyboard" >>~/.bashrc
-
-# set wallpaper and disable lockscreen
-cd /usr/share/backgrounds/
-wget https://raw.githubusercontent.com/mastap666/scripts/master/wallpaper/wp1.jpg
-su kali -m -c 'xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/last-image --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/last-single-image --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/image-path --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/last-image --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor1/last-single-image --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual1/workspace0/last-image --set /usr/share/backgrounds/wp1.jpg
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual1/workspace1/last-image --set /usr/share/backgrounds/wp1.jpg'
-
-
-#set new root password
-if [ $rootpw == y ]
-		then
-			echo "
-
-change root password...
--------------------
-
+echo -e "${yON}<----------------------------------------------------->
+Set Keyboard Layout${cOFF}
 "
-			sleep 2
-			passwd root
-			sleep 5
-			
-fi
-echo "
-
+funcKeylayout
+echo -e "
+${yON}...done${cOFF}
 "
-
-#set new kali password
-if [ $kalipw == y ]
-		then
-			echo "
-
-change kali User password...
--------------------
-
+echo -e "${yON}<----------------------------------------------------->
+Set new Hostname${cOFF}
 "
-			sleep 2
-			passwd kali
-			sleep 5
-			
-fi
-
-
-##install tools
-echo "
-
-
-Default tools will be installed...
-----------------------------------
+funcSethostname
+echo -e "
+${yON}...done${cOFF}
 "
-sleep 3
-apt-get update
-sleep 3
-apt-get install ntpdate -y
-apt-get install terminator -y
-mkdir -p /tools
-
-#update tools
-nmap --script-updatedb
-searchsploit -u
-
-
-#Github Clones
-if [ $git == "y" ]
-		then 
-			echo "			
-Github Tools will be cloned...
-----------------------------------
-
+echo -e "${yON}<----------------------------------------------------->
+Set new Passwords${cOFF}
 "
-sleep 3
-
-mkdir -p /tools/gitclone
-cd /tools/gitclone
-git clone https://github.com/OCSAF/freevulnsearch.git 
-git clone https://github.com/OCSAF/freevulnaudit.git
-git clone https://github.com/OCSAF/free-open-auditor.git
-git clone https://github.com/OCSAF/freepwnedcheck.git
-
-fi
-
-
-if [ $tools == "y" ]
-		then 
-			echo "
-			
-Custom tools will be installed...
-----------------------------------
-
-			"
-			sleep 3
-			#div_tools
-			apt-get install ipcalc -y
-			apt-get install mtr -y
-			apt-get install mtr-tiny -y
-			apt-get install tor -y
-			apt-get install openvas -y
-			#freevulnserach
-			apt-get install geoip-bin -y
-			apt-get install jq -y
-			#freevulnaudit
-			apt-get install xlstproc -y
-			apt-get install wkhtmltopdf -y
-			echo "
-			
-tool installations completed...
-
+funcSetpw
+echo -e "
+${yON}...done${cOFF}
+"
+echo -e "${yON}<----------------------------------------------------->
+Expand Config Filest${cOFF}
+"
+funcConfExpand
+echo -e "
+${yON}...done${cOFF}
+"
+echo -e "${yON}<----------------------------------------------------->
+Set Wallpaper${cOFF}
+"
+funcSetwallpaper
+echo -e "
+${yON}...done${cOFF}
+"
+echo -e "${yON}<----------------------------------------------------->
+Install default tools${cOFF}
+"
+funcDefaulttools
+echo -e "
+${yON}...done${cOFF}
+"
+#echo -e "${yON}<----------------------------------------------------->
+#Update NMAP and Searchsploit${cOFF}
+#"
+#funcUpdatetools
+#echo -e "
+#${yON}...done${cOFF}
+#"
+echo -e "${yON}<----------------------------------------------------->
+Clone Github tools${cOFF}
+"
+funcGithub
+echo -e "
+${yON}...done${cOFF}
+"
+echo -e "${yON}<----------------------------------------------------->
+Install Custom tools${cOFF}
+"
+funcCustomtools
+echo -e "
+${yON}...done${cOFF}
+"
+echo -e "${yON}<----------------------------------------------------->
+Set NTP Server${cOFF}
+"
+funcSetntp
+echo -e "
+${yON}...done${cOFF}
+"
+echo -e "${yON}<----------------------------------------------------->
+Enable SSH Server${cOFF}
+"
+funcSSHenable
+echo -e "
+${yON}...done${cOFF}
+"
+echo -e "${yON}<----------------------------------------------------->
+Set Timezone${cOFF}
+"
+funcTimezone
+echo -e "
+${yON}...done${cOFF}
+"
+echo -e "${yON}<----------------------------------------------------->
+Set Quad9 DNS Server${cOFF}
+"
+funcDNSchange
+echo -e "
+${yON}...done${cOFF}
+"
+echo -e "${yON}<----------------------------------------------------->
+Install Kali Updates${cOFF}
+"
+funcUpdate
+echo -e "
+${yON}...done${cOFF}
 "
 
 
 
-#set ntp server
-echo "
-
-Set ntp server...
------------------
-"
-ntpdate $ntp
-sleep 3
-
-# change hostname
-if [ $hs == y ]
-		then
-			echo "
-
-Set new hostname...
------------------
-"
-			echo $hostname >/etc/hostname
-			echo "
-			
-Your new hostname is $hostname
-
-"
-
-fi
-
-# enable ssh with root access
-if [ $ssh == "y" ]
-        then
-		echo "
-
-Setup SSH Server...
--------------------
-
-"
-			echo SSH will be enabled...
-			echo "PubkeyAuthentication yes" >>/etc/ssh/sshd_config
-			systemctl enable ssh
-			systemctl start ssh
-
-fi
-echo "
-"
-sleep 3
-# set timezone
-timedatectl set-timezone $timezone
-
-
-
-#DNS Quad9 Server
-if [ $dns == y ]
-		then
-				echo "Quad9 DNS will be configured... "
-				echo "nameserver 9.9.9.9" >/etc/resolv.conf
-				
-fi
-
-# update kali
-if [ $update == y ]
-        then
-                echo "Updates will be installed, please hold the line..."
-				sleep 3
-                apt-get update -y && apt-get upgrade -y
-				
-	elif [ $rolling == y ]
-		then
-				echo "Rolling updates will be installed..."
-				apt-get dist-upgrade -y
-                
-	else
-                echo "No updates will be installed..."
-
-fi
-
-
-echo "
+echo -e "${gON}
 
 
 
@@ -293,7 +429,7 @@ your machine will reboot in 1 minute....
 
 
 
-"
+${cOFF}"
 shutdown -r 1
 
 #### END #####
